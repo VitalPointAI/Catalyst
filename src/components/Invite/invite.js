f8104d360b69113ea8eedceefece2b2b7b6ef79e
Em-kale <e.luhning@vitalpoint.ai>
2021-06-08 00:44:26 -0400

    added invite support for facebook, twitter, and outlook

--- src/components/Invite/invite.js
+++ src/components/Invite/invite.js
@@ -5,15 +5,32 @@ import Dialog from '@material-ui/core/Dialog'
 import { DialogTitle } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
-
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import EmailIcon from '@material-ui/icons/Email';
+//other libraries
+import {ShareButton} from "react-custom-share"
 
 const Invite = (props) => {
     const [open, setOpen] = useState(true)
     const { 
         handleInviteClickState,
        } = props
-
-    const link = window.location.hostname + ":1234/invitation" + window.location.pathname 
+   
+    const link = window.location.origin + "/invitation" + window.location.pathname 
+    
+    const FBButtonProps = {
+        url: link, 
+        network: "Facebook",  
+    }
+    const TwitterButtonProps = {
+        url: link, 
+        network: "Twitter",  
+    }
+    const EmailButtonProps = {
+        url: link, 
+        network: "Email",  
+    }
 
     const handleClose = () => {
         handleInviteClickState(false)
@@ -22,7 +39,6 @@ const Invite = (props) => {
    function handleCopy(){
        //this will not work in older browsers
        //window.clipboardData.setData("Text", 'Copy this text to clipboard') would though 
-
         navigator.clipboard.writeText(`${link}`)
         alert("Copied!");
     };
@@ -34,6 +50,9 @@ const Invite = (props) => {
             <TextField style={{width: 400}} placeholder={`${link}`} id="linkField"/>
             <Button onClick={handleClose}>Close</Button> 
             <Button onClick={handleCopy}>Copy</Button>
+            <ShareButton {...FBButtonProps}><FacebookIcon></FacebookIcon></ShareButton>
+            <ShareButton {...TwitterButtonProps}><TwitterIcon></TwitterIcon></ShareButton>
+            <ShareButton {...EmailButtonProps}><EmailIcon></EmailIcon></ShareButton>
         </Dialog>
         </div>
     ); 
