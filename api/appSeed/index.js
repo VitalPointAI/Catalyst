b5fa6e64cec7037c7d11c636d36902faa7c6c77f
Aaron Luhning <a.luhning@vitalpoint.ai>
2022-06-05 15:48:20 -0400

    process

--- api/appSeed/index.js
+++ api/appSeed/index.js
@@ -1,10 +1,10 @@
+const appSeed = process.env.appSeed
+
 module.exports = async function (context, req) {
     context.log('AppSeed trigger function processed a request.');
 
-    const appSeed = '${process.env.appSeed}'
-
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: {appSeed}
+        body: {seed: appSeed}
     };
 }
